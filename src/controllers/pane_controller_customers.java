aeda9287e5d773854b1f0e067f13039eaf0a0c83
ManuelAlonso <1717110243@utectulancingo.edu.mx>
2018-11-07 13:13:56 -0600

    Panel Clientes

---
+++ src/controllers/pane_controller_customers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author ManuelAlonsoMH
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import views.pane_view_customers;
+import models.pane_model_customers;
+
+public class pane_controller_customers implements ActionListener{
+
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
